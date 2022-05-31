7e921cc0dae7255e15b5c220e1b5f4f0dcc72ce7
Saoa35 <saoamah@gmail.com>
2022-05-31 22:15:46 +0200

    + props @ map

--- src/Components/List2/List2.js
+++ src/Components/List2/List2.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Item from "../Item/Item";
 
 export default class List2 extends React.Component {
 
@@ -7,6 +8,10 @@ export default class List2 extends React.Component {
         return(
             <ol>
                 List 2
+
+                {this.props.items.map(element => {
+                    return <Item key={element.id} value={element.item}/>
+                })}
             </ol>
         )
     }
