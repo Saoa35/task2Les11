cb5951f43c089622535ab24c7eb41a0a579a9456
Saoa35 <saoamah@gmail.com>
2022-05-31 22:16:32 +0200

    + state & constructor

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,11 +4,29 @@ import List2 from '../List2/List2';
 import './App.css';
 
 class App extends React.Component {
+
+  state = {
+    items: [],
+    flag: true
+  }
+
+  constructor(props) {
+    super(props);
+
+  }
+
   render() {
     return (
       <div className="App">
-       <List1/>
-       <List2/>
+
+        <button>Change List Type</button>
+
+        <button>Add to end</button>
+
+        <button>Add to start</button>
+
+       {this.state.flag ? <List1 items={this.state.items}/> : <List2 items={this.state.items}/>}
+
       </div>
     );
   }
