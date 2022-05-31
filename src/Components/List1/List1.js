4e93922bc594b44e66ec4b4acb979cde6103b5af
Saoa35 <saoamah@gmail.com>
2022-05-31 21:38:05 +0200

    create List1 component

---
+++ src/Components/List1/List1.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class List1 extends React.Component {
+
+    render() {
+
+        return(
+            <ul>
+                List 1
+            </ul>
+        )
+    }
+}
