a4d3ceee7b1e5939def95755036c07561a58d0fa
Saoa35 <saoamah@gmail.com>
2022-05-31 21:44:26 +0200

    change func comp to class comp

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,17 @@
+import React from 'react';
 import List1 from '../List1/List1';
 import List2 from '../List2/List2';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <List1/>
-     <List2/>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+       <List1/>
+       <List2/>
+      </div>
+    );
+  }
 }
 
 export default App;
