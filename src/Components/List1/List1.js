ca3d640b9777f77d1a5b12398f5c3d5980d6c51e
Saoa35 <saoamah@gmail.com>
2022-06-01 17:48:44 +0200

    wrap list 1, 2 to h2

--- src/Components/List1/List1.js
+++ src/Components/List1/List1.js
@@ -7,7 +7,9 @@ export default class List1 extends React.Component {
 
         return(
             <ul>
-                List 1
+                <h2>
+                    List 1
+                </h2>
 
                 {this.props.items.map(element => {
                     return <Item key={element.id} value={element.item}/>
--- src/Components/List2/List2.js
+++ src/Components/List2/List2.js
@@ -7,7 +7,9 @@ export default class List2 extends React.Component {
 
         return(
             <ol>
-                List 2
+                <h2>
+                 List 2
+                </h2>
 
                 {this.props.items.map(element => {
                     return <Item key={element.id} value={element.item}/>
