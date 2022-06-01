5abfca7a4a9d76981ec38d89d3a8ad53b3583917
Saoa35 <saoamah@gmail.com>
2022-06-01 17:22:50 +0200

    + for loop

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import List1 from '../List1/List1';
 import List2 from '../List2/List2';
 import './App.css';
+import faker from 'faker';
 
 class App extends React.Component {
 
@@ -13,6 +14,12 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
+    for(let i = 0; i < 10; i++) {
+      this.state.items.push({
+        item: faker.internet.userName()
+      })
+    }
+
   }
 
   render() {
@@ -20,11 +27,11 @@ class App extends React.Component {
       <div className="App">
 
         <button>Change List Type</button>
-
+          <br/>
         <button>Add to end</button>
-
+          <br/>
         <button>Add to start</button>
-
+          <br/>
        {this.state.flag ? <List1 items={this.state.items}/> : <List2 items={this.state.items}/>}
 
       </div>
