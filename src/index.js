e872263e4c7e38562c0f9c5049a7ea9cf2980f61
Saoa35 <saoamah@gmail.com>
2022-06-02 14:58:45 +0200

    change react render

--- src/index.js
+++ src/index.js
@@ -2,11 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App/App';
+import { apiResponsce } from './apiResponsce';
+import { createStore } from 'redux';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const reducer = (state = apiResponsce, action) => {
+  if(action.type === 'LIKE_COMMENT') {
+    let comments = state.comments.slice(0);
+    comments = comments.map(c => {
+      const newComment = Object.assign({}, c);
+      if(c.id === action.commentID) {
+        newComment.likes += 1;
+      }
+      return newComment;
+    })
+
+    return Object.assign({}, state, {comments: comments})
+  }
+  return state;
+};
+
+const store = createStore(reducer);
+window.store = store;
+
+
+const renderApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      {/* <App /> */}
+    </React.StrictMode>
+    document.getElementById('root')
+  )
+}
+
+renderApp();
+
+store.subscribe(renderApp)
