b2e85b5a0ffa75083608fe35a7da191694e5e2e0
Saoa35 <saoamah@gmail.com>
2022-06-02 12:12:55 +0200

    create apiResponsce

---
+++ src/apiResponsce.js
@@ -0,0 +1,37 @@
+export const apiResponsce = {
+    id: 12372,
+    lessonName: 'Redux constraction',
+    description: 'Lorem inspum some words',
+    comments: [
+        {
+            id: 1,
+            likes: 2,
+            dislikes: 0,
+            text: 'Great!...',
+            author: {
+                id: 2133,
+                name: 'Alex'
+            }
+        },
+        {
+            id: 2,
+            likes: 3,
+            dislikes: 1,
+            text: 'super...',
+            author: {
+                id: 3355,
+                name: 'John'
+            }
+        },
+        {
+            id: 3,
+            likes: 0,
+            dislikes: 1,
+            text: 'Wow...',
+            author: {
+                id: 82,
+                name: 'Alan'
+            }
+        }
+    ]
+};
