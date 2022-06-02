91022e5a0152fda86f63c7af9c4da0f82ba7aef2
Saoa35 <saoamah@gmail.com>
2022-06-02 19:57:05 +0200

    change reduser

--- src/index.js
+++ src/index.js
@@ -1,23 +1,55 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Components/App/App';
+// import App from './Components/App/App';
 import { apiResponsce } from './apiResponsce';
 import { createStore } from 'redux';
+import JSONPretty from 'react-json-pretty';
+import { normalize, schema } from 'normalizr';
 
 
-const reducer = (state = apiResponsce, action) => {
+const user = new schema.Entity('users');
+const comment = new schema.Entity('comments', {
+  author: user
+});
+
+const lesson = new schema.Entity('lesson', {
+  comments: [ comment ]
+})
+
+const normalizedData = normalize(apiResponsce, lesson);
+
+// const reducer = (state = apiResponsce, action) => {
+//   if(action.type === 'LIKE_COMMENT') {
+//     let comments = state.comments.slice(0);
+//     comments = comments.map(c => {
+//       const newComment = Object.assign({}, c);
+//       if(c.id === action.commentID) {
+//         newComment.likes += 1;
+//       }
+//       return newComment;
+//     })
+
+//     return Object.assign({}, state, {comments: comments})
+//   }
+//   return state;
+// };
+
+const reducer = (state = normalizedData.entities, action) => {
   if(action.type === 'LIKE_COMMENT') {
-    let comments = state.comments.slice(0);
-    comments = comments.map(c => {
-      const newComment = Object.assign({}, c);
-      if(c.id === action.commentID) {
-        newComment.likes += 1;
-      }
-      return newComment;
-    })
+    const { comments } = state;
+    const { commentID: id } = action;
 
-    return Object.assign({}, state, {comments: comments})
+    return {
+      ...state,
+      comments: {
+        ...comments,
+        [id]: {
+          ...comments[id],
+          likes: comments[id].likes + 1
+        }
+      }
+    }
   }
   return state;
 };
@@ -30,11 +62,12 @@ const renderApp = () => {
   ReactDOM.render(
     <React.StrictMode>
       {/* <App /> */}
-    </React.StrictMode>
+      <JSONPretty json={store.getState()}/>
+    </React.StrictMode>,
     document.getElementById('root')
   )
 }
 
 renderApp();
 
-store.subscribe(renderApp)
+store.subscribe(renderApp);
